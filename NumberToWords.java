72091a2c6f93970fe750f623f484a3d5a001d708
Raj Mohan <rajmohan2695@gmail.com>
2019-07-26 13:41:09 +0530

    Have to find and fix the two digits in thousands, ex: Twenty Five Thousand

--- NumberToWords.java
+++ NumberToWords.java
@@ -2,29 +2,47 @@ import java.util.ArrayList;
 
 public class NumberToWords {
     public static void main(String[] args) {
-        String[] num={"","one","two","three","four","five","six","seven","eight","nine"};
-        String[] tens={"","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-        String[] twodigits={"","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"};
-        String[] places={"hundred","thousand","lakhs","crore"};
 
-//        String[][] places={{"","one","two","three","four","five","six","seven","eight","nine"},
-//                {"","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"},
-//                {"","eleven","twelve","thirteen","fourteen","fifteen","sixteen","seventeen","eighteen","nineteen"},
-//                {"hundred","thousand","lakhs","crore"}};
+        String[] num = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        String[] tens = {"", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+        String[] twodigits = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+        String[] hundred = {"", "hundred and"};
+        String[] thousand = {"", "thousand"};
+        String[] lakhs = {"", "lakhs"};
+        String[] crore = {"", "crore"};
+
+//        String[] num = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+//        String[] tens = {"", "ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+//        String[] twodigits = {"", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+//        String[] hundred = {"", "hundred and"};
+//        String[] thousand = {"", "thousand"};
+//        String[] lakhs = {"", "lakhs"};
+//        String[] crore = {"", "crore"};
+
+        String[][] places={{"","one","two","three","four","five","six","seven","eight","nine"},
+                {"","ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"},
+                {"hundred","thousand","lakhs","crore"}};
 
 
         int number = 123643;
-        int[] digit=new int[8];
-        int a=-1;
-        while(number!=0){
+        int[] digit = new int[8];
+        int hold[] = {0, 0, 0, 0, 0, 0, 0, 0};
+        int a = -1;
+        while (number != 0) {
             a++;
-            digit[a] = number%10;
-            number=number/10;
+            digit[a] = number % 10;
+            hold[a] = 1;
+            number = number / 10;
         }
-        for (int i=0;i<=a;i++) {
-            System.out.println(num[digit[i]]);
+        String result = "";
+        int i = 0;
+        if ((hold[1] == 1) && (digit[1] == 1)) {
+            result = tens[digit[0]];
+            i = 2;
         }
-
-
+        System.out.println();
+        for (; i <= a; i++) {
+            //result=
+        }
     }
-}
+    }
