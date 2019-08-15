c98db5058ea05dfd360b53922d5ff8e179763903
Raj Mohan <rajmohan2695.com>
2019-08-15 12:33:57 +0530

    Implementation of Stack

---
+++ Data_Structures_And_Algorithms/Stack/App.java
@@ -0,0 +1,12 @@
+package Data_Structures_And_Algorithms.Stack;
+
+public class App {
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(5);
+        s.push(10);
+        s.pop();
+        s.push(34);
+        s.display();
+    }
+}
---
+++ Data_Structures_And_Algorithms/Stack/Stack.java
@@ -0,0 +1,22 @@
+package Data_Structures_And_Algorithms.Stack;
+
+public class Stack {
+    int k = 5;
+    int i = -1;
+    int[] stack = new int[k];
+    public Stack (){
+
+    }
+    public void push(int a){
+        i=i+1;
+        stack[i] = a;
+    }
+    public void pop(){
+        --i;
+    }
+    public void display(){
+        for(int l=i;l>=0;l--){
+            System.out.println(stack[l]);
+        }
+    }
+}
