5f0a0c3673aacc6b177f122fd97a3cb6ca51b98b
Raj Mohan <rajmohan2695.com>
2019-08-16 20:54:56 +0530

    Minor Change

--- Data_Structures_And_Algorithms/Stack/Stack.java
+++ Data_Structures_And_Algorithms/Stack/Stack.java
@@ -2,20 +2,20 @@ package Data_Structures_And_Algorithms.Stack;
 
 public class Stack {
     int k = 5;
-    int i = -1;
+    int current = -1;
     int[] stack = new int[k];
     public Stack (){
 
     }
     public void push(int a){
-        i++;
-        stack[i] = a;
+        current++;
+        stack[current] = a;
     }
     public void pop(){
-        --i;
+        --current;
     }
     public void display(){
-        for(int l=i;l>=0;l--){
+        for(int l=current;l>=0;l--){
             System.out.println(stack[l]);
         }
     }
