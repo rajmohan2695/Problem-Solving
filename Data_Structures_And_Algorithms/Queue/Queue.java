1a3e257b538db7ad12f53fde6170f741c2ef1028
rajmohan2695 <rajmohan2695@gmail.com>
2019-08-17 14:25:14 +0530

    creating display the queue method

--- Data_Structures_And_Algorithms/Queue/Queue.java
+++ Data_Structures_And_Algorithms/Queue/Queue.java
@@ -2,29 +2,44 @@ package Data_Structures_And_Algorithms.Queue;
 
 public class Queue {
     private int current = -1;
-    private int end = -1;
+    private int qsize = -1;
     private int[] queue;
     private int size;
-    public Queue(int size){
+
+    public Queue(int size) {
         queue = new int[size];
         this.size = size;
     }
 
-    public void push(int data){
+    public void push(int data) {
         current++;
-        queue[current%size] = data;
-        end = (end+1)%size;
+        queue[current % size] = data;
+        qsize = (qsize + 1) % size;
     }
 
-    public void pop(){
-        current++;
-        size--;
+    public void pop() {
+        if (isEmpty()) {
+            System.out.println("Queue is Empty");
+        } else {
+            current++;
+            qsize--;
+        }
+    }
+
+    public void displayall() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(queue[i]);
+        }
     }
 
     public void display(){
-        for(int i=0;i<size;i++){
-            System.out.println(queue[i]);
+        for(int i=0,j=current;i<qsize;i++,j++){
+            System.out.println(queue[j%size]);
         }
         System.out.println();
+    }
+
+    public boolean isEmpty() {
+        return qsize == -1;
     }
 }
