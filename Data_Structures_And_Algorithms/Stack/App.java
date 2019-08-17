7aba3712767be96709b7d84d22b88de2caf199cf
rajmohan2695 <rajmohan2695@gmail.com>
2019-08-17 10:43:06 +0530

    Solving Index out of bounds

--- Data_Structures_And_Algorithms/Stack/App.java
+++ Data_Structures_And_Algorithms/Stack/App.java
@@ -2,10 +2,18 @@ package Data_Structures_And_Algorithms.Stack;
 
 public class App {
     public static void main(String[] args) {
-        Stack s = new Stack();
+        Stack s = new Stack(5);
         s.push(5);
         s.push(10);
         s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.pop();
+        s.push(34);
+        s.push(34);
+        s.push(34);
         s.push(34);
         s.push(35);
         s.display();
--- Data_Structures_And_Algorithms/Stack/Stack.java
+++ Data_Structures_And_Algorithms/Stack/Stack.java
@@ -1,22 +1,45 @@
 package Data_Structures_And_Algorithms.Stack;
 
 public class Stack {
-    int k = 5;
-    int current = -1;
-    int[] stack = new int[k];
-    public Stack (){
+    private int current = -1;
+    private int size;
+    private int[] stack;
 
+    public Stack(int size) {
+        this.size = size;
+        stack = new int[size];
     }
-    public void push(int a){
-        current++;
-        stack[current] = a;
+
+    public void push(int a) {
+        if (!isFull()) {
+            System.out.print("");
+            current++;
+            stack[current] = a;
+        } else {
+            System.out.println("Limit Exceeded");
+        }
     }
-    public void pop(){
-        --current;
+
+    public void pop() {
+        if (!isEmpty()) {
+            System.out.print("");
+            --current;
+        } else {
+            System.out.println("No elements to delete");
+        }
     }
-    public void display(){
-        for(int l=current;l>=0;l--){
+
+    public void display() {
+        for (int l = current; l >= 0; l--) {
             System.out.println(stack[l]);
         }
+    }
+
+    public boolean isEmpty() {
+        return (current == -1);
+    }
+
+    public boolean isFull() {
+        return (current == size - 1);
     }
 }
