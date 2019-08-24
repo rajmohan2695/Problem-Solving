1a9469e46a130cee3341a8bc27dcb00475a22586
rajmohan2695 <rajmohan2695@gmail.com>
2019-08-24 10:14:57 +0530

    Implemeted Linear Search

--- Data_Structures_And_Algorithms/LinearSearch/App.java
+++ Data_Structures_And_Algorithms/LinearSearch/App.java
@@ -1,4 +1,9 @@
 package Data_Structures_And_Algorithms.LinearSearch;
 
 public class App {
+    public static void main(String[] args) {
+        int arr[] = {3,5,3,23,4,52,6,411,323,6,7,87,45,34};
+        LinearSearch s = new LinearSearch();
+        System.out.println(s.Search(arr,323));
+    }
 }
--- Data_Structures_And_Algorithms/LinearSearch/LinearSearch.java
+++ Data_Structures_And_Algorithms/LinearSearch/LinearSearch.java
@@ -1,4 +1,17 @@
 package Data_Structures_And_Algorithms.LinearSearch;
 
 public class LinearSearch {
+    public LinearSearch() {
+
+    }
+
+    public int Search(int[] elements, int elementToFind) {
+        int e = -1;
+        for (int i = 0; i < elements.length; i++) {
+            if (elements[i] == elementToFind) {
+                return i;
+            }
+        }
+        return e;
+    }
 }
