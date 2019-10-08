3287af49df43ff972de39eb973852b7d7c40ef74
unknown <rajmohan2695@gmail.com>
2019-10-08 16:26:58 +0530

    Inheritance in Java POC

---
+++ Code_Workouts/JavaWorkouts/Inheritance/App.java
@@ -0,0 +1,12 @@
+package Code_Workouts.JavaWorkouts.Inheritance;
+
+public class App {
+    public static void main(String[] args) {
+        Parent p = new Parent();
+        Child c = new Child();
+
+        p.information();
+        System.out.println();
+        c.informationc();
+    }
+}
---
+++ Code_Workouts/JavaWorkouts/Inheritance/Parent.java
@@ -0,0 +1,18 @@
+package Code_Workouts.JavaWorkouts.Inheritance;
+
+public class Parent {
+    int agep = 50;
+    String namep = "John";
+    int networthp = 25000;
+
+    public int asserts(){
+        return networthp;
+    }
+
+    public void information(){
+        System.out.println("Name : "+namep);
+        System.out.println("Age : "+agep);
+        System.out.println("Networth : "+networthp);
+    }
+
+}
