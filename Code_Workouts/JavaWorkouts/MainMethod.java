d435adcccc6d00a26e6de59f8d9c1290898fb10f
unknown <rajmohan2695@gmail.com>
2019-10-09 11:22:11 +0530

    Calling main Methid recursively

---
+++ Code_Workouts/JavaWorkouts/MainMethod.java
@@ -0,0 +1,8 @@
+package Code_Workouts.JavaWorkouts;
+// Calling main() recursively
+public class MainMethod {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        main(args);
+    }
+}
