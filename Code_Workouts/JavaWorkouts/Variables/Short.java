d65b3b6ef5b0c516e668e74029d829be77811c5f
unknown <rajmohan2695@gmail.com>
2019-10-10 17:56:27 +0530

    Short variable

--- Code_Workouts/JavaWorkouts/Variables/Short.java
+++ Code_Workouts/JavaWorkouts/Variables/Short.java
@@ -1,4 +1,10 @@
 package Code_Workouts.JavaWorkouts.Variables;
 
 public class Short {
+    public static void main(String[] args) {
+        short a = 10;
+        short b = 20;
+        short c = (short)(a+b);
+        System.out.println(c);
+    }
 }
