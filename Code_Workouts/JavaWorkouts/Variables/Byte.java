a7c375e26cbc6c02e63982401b01e971f2a4e5a9
unknown <rajmohan2695@gmail.com>
2019-10-10 14:30:32 +0530

    Worked on 'byte' Variable

---
+++ Code_Workouts/JavaWorkouts/Variables/Byte.java
@@ -0,0 +1,13 @@
+package Code_Workouts.JavaWorkouts.Variables;
+
+public class Byte {
+    public static void main(String[] args) {
+        byte a = 10;
+        byte b = 20;
+        byte c = (byte) (a + b);
+        System.out.println(c);
+        int d = 130;
+        byte e = (byte) d;
+        System.out.println(e);
+    }
+}
