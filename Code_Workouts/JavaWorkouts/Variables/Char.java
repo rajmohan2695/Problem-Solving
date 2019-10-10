a0e20f313604e6739c1283b903adc639a522e23f
unknown <rajmohan2695@gmail.com>
2019-10-10 14:31:07 +0530

    Worked on Char variable

---
+++ Code_Workouts/JavaWorkouts/Variables/Char.java
@@ -0,0 +1,10 @@
+package Code_Workouts.JavaWorkouts.Variables;
+
+public class Char {
+    public static void main(String[] args) {
+        char g = '\u0000';    // \u0000 is the default value of char in Java
+        System.out.print("A");
+        System.out.print(g);
+        System.out.print("B");
+    }
+}
