6f7eb196afba2b94e64d458f2c2048ac27136084
unknown <rajmohan2695@gmail.com>
2019-10-12 12:14:27 +0530

    Using singleton class

--- Code_Workouts/JavaWorkouts/SingletonClass/App.java
+++ Code_Workouts/JavaWorkouts/SingletonClass/App.java
@@ -1,4 +1,13 @@
 package Code_Workouts.JavaWorkouts.SingletonClass;
 
 public class App {
+    public static void main(String[] args) {
+        SingletonClass obj = SingletonClass.objectCreation();
+        obj.display();
+    }
 }
+
+/*
+        Output:
+        Object for Singleton Class is created and tested
+ */
--- Code_Workouts/JavaWorkouts/SingletonClass/SingletonClass.java
+++ Code_Workouts/JavaWorkouts/SingletonClass/SingletonClass.java
@@ -1,4 +1,19 @@
 package Code_Workouts.JavaWorkouts.SingletonClass;
 
 public class SingletonClass {
+    static SingletonClass obj = null;
+
+    private SingletonClass() {
+        //Made Constructor Private
+    }
+
+    public static SingletonClass objectCreation() {
+        if (obj == null)
+            return new SingletonClass();
+        return obj;
+    }
+
+    public void display() {
+        System.out.println("Object for Singleton Class is created and tested");
+    }
 }
