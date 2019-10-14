dd8e4f587ee1016ba768b037acc3e1b518d8e343
unknown <rajmohan2695@gmail.com>
2019-10-14 15:43:15 +0530

    Using this as arguments in Methods

--- Code_Workouts/JavaWorkouts/ThisKeyword/ArgumentInMethods/App.java
+++ Code_Workouts/JavaWorkouts/ThisKeyword/ArgumentInMethods/App.java
@@ -1,4 +1,8 @@
 package Code_Workouts.JavaWorkouts.ThisKeyword.ArgumentInMethods;
 
 public class App {
+    public static void main(String[] args) {
+        ClassC object = new ClassC();
+        object.method1();
+    }
 }
--- Code_Workouts/JavaWorkouts/ThisKeyword/ArgumentInMethods/ClassC.java
+++ Code_Workouts/JavaWorkouts/ThisKeyword/ArgumentInMethods/ClassC.java
@@ -1,4 +1,11 @@
 package Code_Workouts.JavaWorkouts.ThisKeyword.ArgumentInMethods;
 
 public class ClassC {
+    public void method2(ClassC arg){
+        System.out.println("Method executed getting 'this' as argument");
+    }
+
+    public void method1(){
+        method2(this);
+    }
 }
