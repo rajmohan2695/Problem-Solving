58d37b8269564c5dd7431ec1d0ede06e8f048e48
unknown <rajmohan2695@gmail.com>
2019-10-14 18:39:11 +0530

    Minor Change to Exception Class

--- Code_Workouts/JavaWorkouts/ExceptionHandling/CustomException/InvalidAgeException.java
+++ Code_Workouts/JavaWorkouts/ExceptionHandling/CustomException/InvalidAgeException.java
@@ -1,6 +1,6 @@
 package Code_Workouts.JavaWorkouts.ExceptionHandling.CustomException;
 
-public class InvalidAgeException extends Exception {
+public class InvalidAgeException extends RuntimeException {
     InvalidAgeException(String str) {
         super(str);
     }
