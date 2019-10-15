f70ab988c4cfc04742e3ef9e45b9810d1662f5d2
unknown <rajmohan2695@gmail.com>
2019-10-15 17:09:51 +0530

    LinkedList Workout

--- Code_Workouts/JavaWorkouts/Collections/Listt/LinkedListt/App.java
+++ Code_Workouts/JavaWorkouts/Collections/Listt/LinkedListt/App.java
@@ -1,4 +1,23 @@
 package Code_Workouts.JavaWorkouts.Collections.Listt.LinkedListt;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 public class App {
+    public static void main(String[] args) {
+        LinkedList ll = new LinkedList();
+
+        ll.add(5);
+        ll.add(10);
+        ll.add(20);
+        ll.add(25);
+        ll.add(30);
+        ll.add(2,15);
+
+        Iterator itr = ll.iterator();
+
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
