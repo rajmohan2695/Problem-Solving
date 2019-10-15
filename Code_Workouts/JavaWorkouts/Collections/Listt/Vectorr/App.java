7b37007708a8f17624c881a0be6d480ca2cbc8f3
unknown <rajmohan2695@gmail.com>
2019-10-15 17:10:07 +0530

    Vector Workout

--- Code_Workouts/JavaWorkouts/Collections/Listt/Vectorr/App.java
+++ Code_Workouts/JavaWorkouts/Collections/Listt/Vectorr/App.java
@@ -1,4 +1,21 @@
 package Code_Workouts.JavaWorkouts.Collections.Listt.Vectorr;
 
+import java.util.Iterator;
+import java.util.Vector;
+
 public class App {
+    public static void main(String[] args) {
+        Vector v = new Vector();
+        v.add(1);
+        v.add(2);
+        v.add(3);
+        v.add(4);
+        v.add(5);
+
+        Iterator itr = v.iterator();
+
+        while(itr.hasNext()){
+            System.out.println(itr.next());
+        }
+    }
 }
