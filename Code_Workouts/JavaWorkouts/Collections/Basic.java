176352ceed28575303b3b983a0527f2d7e0d1dc8
unknown <rajmohan2695@gmail.com>
2019-10-16 20:11:43 +0530

    Basic Workouts

--- Code_Workouts/JavaWorkouts/Collections/Basic.java
+++ Code_Workouts/JavaWorkouts/Collections/Basic.java
@@ -1,5 +1,6 @@
 package Code_Workouts.JavaWorkouts.Collections;
 
+import java.sql.Array;
 import java.util.HashMap;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -7,6 +8,8 @@ import java.util.Queue;
 public class Basic {
     public static void main(String[] args) {
         HashMap<String,String> map = new HashMap<>();
+
         PriorityQueue q = new PriorityQueue();
+
     }
 }
