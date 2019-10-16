0b22a174591bc8a97dd7eb177e40863fb16dcfb6
unknown <rajmohan2695@gmail.com>
2019-10-16 20:11:18 +0530

    Worked out with Comparable

--- Code_Workouts/JavaWorkouts/Collections/Comparable/App.java
+++ Code_Workouts/JavaWorkouts/Collections/Comparable/App.java
@@ -1,4 +1,23 @@
 package Code_Workouts.JavaWorkouts.Collections.Comparable;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class App {
+
+    public static void main(String[] args) {
+        ArrayList<Movie> list = new ArrayList<Movie>();
+        list.add(new Movie("Lilly",4.5,1977));
+        list.add(new Movie("Micheal",3.5,1956));
+        list.add(new Movie("Calvin",4.8,1972));
+        list.add(new Movie("Rose Inn",5.0,2018));
+
+        Collections.sort(list);
+
+        for(Movie movie:list){
+            System.out.println(movie.getMovie()+" "+movie.getRating()+" "+movie.getYear());
+        }
+    }
+
+
 }
--- Code_Workouts/JavaWorkouts/Collections/Comparable/Movie.java
+++ Code_Workouts/JavaWorkouts/Collections/Comparable/Movie.java
@@ -1,4 +1,46 @@
 package Code_Workouts.JavaWorkouts.Collections.Comparable;
 
-public class Movie {
+import java.util.*;
+
+public class Movie implements Comparable<Movie> {
+
+    private String movie;
+    private double rating;
+    private int year;
+
+    @Override
+    public int compareTo(Movie movie) {
+        return this.year - movie.year;
+    }
+
+    public String getMovie() {
+        return movie;
+    }
+
+    public void setMovie(String movie) {
+        this.movie = movie;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    Movie(String movie, double rating, int year){
+        this.movie = movie;
+        this.rating = rating;
+        this.year = year;
+    }
+
 }
