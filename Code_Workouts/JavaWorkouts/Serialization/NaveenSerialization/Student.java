fe389f54280f5b94026fcc3341a9875a77e3e529
unknown <rajmohan2695@gmail.com>
2019-10-19 23:08:37 +0530

    Working on Serialization

--- Code_Workouts/JavaWorkouts/Serialization/NaveenSerialization/Student.java
+++ Code_Workouts/JavaWorkouts/Serialization/NaveenSerialization/Student.java
@@ -1,13 +1,6 @@
 package Code_Workouts.JavaWorkouts.Serialization.NaveenSerialization;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.HashMap;
 
 public class Student implements Serializable {
@@ -16,11 +9,11 @@ public class Student implements Serializable {
 	String fileName = "obj.txt";
 
 	public Student() {
-		map.put(189, "one");
-		map.put(9, "two");
-		map.put(800, "three");
-		map.put(7, "four");
-		map.put(6, "five");
+		map.put(1, "one");
+		map.put(2, "two");
+		map.put(3, "three");
+		map.put(4, "four");
+		map.put(5, "five");
 	}
 
 	public void serializable() {
@@ -28,30 +21,20 @@ public class Student implements Serializable {
 			FileOutputStream file = null;
 			ObjectOutputStream out = null;
 			File f = new File(fileName);
-			file = new FileOutputStream(fileName);
+			if (f.exists()){
+				int temp=0;
+				while(temp<5) {
+					map.put(count++,temp++ +" ");
+				}
+			}
+            file = new FileOutputStream(fileName);
 			out = new ObjectOutputStream(file);
 			out.writeObject(map);
-//			if (f.exists()){
-//				int temp=0;
-//				while(temp<10) {
-//					map.put(count++,temp++ +" ");
-			map.put(11, "one");
-			map.put(65, "two");
-			map.put(6676, "three");
-			map.put(4563, "four");
-			map.put(975, "five");
-//				}
 			out.close();
 			file.close();
 			System.out.println("object has been serialised");
-		}
-//			file = new FileOutputStream(fileName);
-//			out = new ObjectOutputStream(file);
-//			out.writeObject(map);
 
-
-//	}
-	catch (Exception e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -59,11 +42,12 @@ public class Student implements Serializable {
 
 	public void deSerialise() {
 		try {
-			FileInputStream input = new FileInputStream(fileName);
-			ObjectInputStream inu = new ObjectInputStream(input);
+            FileInputStream input = new FileInputStream(fileName);
+            ObjectInputStream inu = new ObjectInputStream(input);
 			HashMap<Integer, String> hash = (HashMap) inu.readObject();
+
 			System.out.println("object has been deserialised");
-			System.out.println(hash);
+			this.map = hash;
 			inu.close();
 			input.close();
 		} catch (IOException | ClassNotFoundException e) {
