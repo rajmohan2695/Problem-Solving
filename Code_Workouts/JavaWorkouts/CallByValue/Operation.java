1b90a22decaa7f813195ad7127019c46bcaa600c
unknown <rajmohan2695@gmail.com>
2019-10-25 14:15:35 +0530

    Java call by value workouts

--- Code_Workouts/JavaWorkouts/CallByValue/Operation.java
+++ Code_Workouts/JavaWorkouts/CallByValue/Operation.java
@@ -6,10 +6,20 @@ public class Operation {
         data = data + 20;
     }
 
+    public void increment(Operation op){
+        op.data = op.data+100;
+    }
     public static void main(String[] args) {
         Operation op = new Operation();
         System.out.println("Before increment : "+op.data);
         op.increment(100);
         System.out.println("After increment : "+op.data);
+
+        System.out.println("------------------------");
+
+        System.out.println("Before increment : "+op.data);
+        op.increment(op);
+        System.out.println("After increment : "+op.data);
+
     }
 }
