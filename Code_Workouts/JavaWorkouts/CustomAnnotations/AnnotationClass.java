e85d868df8cfe6de8b2078f8d4a50a09756cfd9e
unknown <rajmohan2695@gmail.com>
2019-11-08 20:39:19 +0530

    Working on custom annotations

---
+++ Code_Workouts/JavaWorkouts/CustomAnnotations/AnnotationClass.java
@@ -0,0 +1,13 @@
+package Code_Workouts.JavaWorkouts.CustomAnnotations;
+
+public class AnnotationClass {
+    @interface Car{
+        String name() default "Lanzer";
+        String colour() default "White";
+    }
+
+    @Car(colour = "Blue")
+    public void myCar(){
+        System.out.println(colour);
+    }
+}
