7a3cee654ecabc6d5f5c232e3a6179b54c025431
unknown <rajmohan2695@gmail.com>
2019-11-14 06:52:28 +0530

    Efficiency rewrite

--- Problem_Solving/FindingLargestPrimeNumber.java
+++ Problem_Solving/FindingLargestPrimeNumber.java
@@ -2,11 +2,12 @@ package Problem_Solving;
 
 public class FindingLargestPrimeNumber {
     public static void main(String[] args) {
-        int n = 1, temp1 = 0;
+
         String str = "156711";
         int max = 0;
-        int tempMax, temp;
-//		int a=n;
+        int tempMax;
+
+        breakall:
         for (int i = str.length() - 1; i >= 0; i--) {
             for (int j = 0; j + i < str.length(); j++) {
                 String tempStr = str.substring(j, j + i + 1);
@@ -16,19 +17,21 @@ public class FindingLargestPrimeNumber {
                 }
             }
             if (max != 0) {
-                System.out.println(max);
-                break;
+                System.out.println("The largest prime number is : "+max);
+                break breakall;
             }
+        }
+        if(max==0){
+            System.out.println("No prime number found in the given number");
         }
 
     }
 
     public static boolean prime(int n) {
-        // Corner case
+
         if (n <= 1)
             return false;
 
-        // Check from 2 to n-1
         for (int i = 2; i < n; i++)
             if (n % i == 0)
                 return false;
@@ -36,4 +39,3 @@ public class FindingLargestPrimeNumber {
         return true;
     }
 }
-
