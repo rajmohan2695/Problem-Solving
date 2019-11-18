134e5a9115d157c409fbfb8952bfc38a6eb54946
unknown <rajmohan2695@gmail.com>
2019-11-18 19:46:12 +0530

    creating a div using DOM manipulation and append some texts inside

--- Code_Workouts/JavascriptWorkouts/DOMmanipulation/DOMmanipulation.js
+++ Code_Workouts/JavascriptWorkouts/DOMmanipulation/DOMmanipulation.js
@@ -30,3 +30,46 @@ console.log(c[2].innerText);
 */
 
 /*---------------------------------------------------------------*/
+
+//document.querySelector is used to select the firsr instance of the element
+var d = document.querySelector('li');
+console.log(typeof(d));
+console.log(d.innerText);
+
+/* Output
+	object
+	Content 1
+*/
+
+/*---------------------------------------------------------------*/
+
+//document.querySelectorAll is used to get all the elements from the given element and keeo it in an object
+var e = document.querySelectorAll('li');
+console.log(typeof(e));
+console.log(e);
+
+/* Output
+	object
+	NodeList(5) [li.a, li.a, li.a, li.a, li.a]
+*/
+
+/*---------------------------------------------------------------*/
+
+//creating a tag and inputting the elements in it using DOM manipulation
+var f = document.createElement('div');
+var g = document.createTextNode('Hello World! I\'ve a working DOM');
+f.appendChild(g);
+document.body.appendChild(f);
+
+/* Output
+	Hi, This is Awesome
+	Content 1
+	Content 2
+	Content 3
+	Content 4
+	Content 5
+	Hello World! I've a working DOM
+*/
+
+/*---------------------------------------------------------------*/
+
