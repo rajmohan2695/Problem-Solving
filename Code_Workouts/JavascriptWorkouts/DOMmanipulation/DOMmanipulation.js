bd93c8ef60a57712527e5e9c2cdf08ae482865c8
unknown <rajmohan2695@gmail.com>
2019-11-18 16:09:12 +0530

    DOM Manipulation - getElementsByClassName

--- Code_Workouts/JavascriptWorkouts/DOMmanipulation/DOMmanipulation.js
+++ Code_Workouts/JavascriptWorkouts/DOMmanipulation/DOMmanipulation.js
@@ -1,4 +1,3 @@
-
 //innerText displays only the 'text' present inside the element
 var a = document.getElementById('content').innerText;
 console.log(a);
@@ -7,6 +6,8 @@ console.log(a);
 	Hi, This is Awesome
 */
 
+/*---------------------------------------------------------------*/
+
 //innerHTML displays all the content present inside the element including the comments
 var b = document.getElementById('content').innerHTML;
 console.log(b);
@@ -14,6 +15,18 @@ console.log(b);
 /* Output 
 	<!-- innerHTML vs innerText -->
 	Hi, This is <b> Awesome </b>
+*/
+
+/*---------------------------------------------------------------*/
+
+//document.getElementByClassName 
+var c = document.getElementsByClassName('a');
+console.log(typeof(c));
+console.log(c[2].innerText);
+
+/*	Output
+	object
+	VM80:3 Content 3
 */
 
 /*---------------------------------------------------------------*/
