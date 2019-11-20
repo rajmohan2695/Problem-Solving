521680ef88271baff50bdc2dd7e000e8d7376799
unknown <rajmohan2695@gmail.com>
2019-11-20 15:19:38 +0530

    Function callback-2

--- Code_Workouts/JavascriptWorkouts/functions.js
+++ Code_Workouts/JavascriptWorkouts/functions.js
@@ -8,3 +8,24 @@ var x = function(callback){
 var y = function(){
 	console.log("Hi");
 }
+
+//function callbacks - 2
+let add = function(x,y){
+	return x+y;
+}
+
+let mul = function(x,y){
+	return x*y;
+}
+
+let doWhatEver = function(x,y){
+	return `here are the two numbers ${x} and ${y}`;
+}
+
+let calc = function(x,y,callback){
+	return callback(x,y);
+}
+
+console.log(calc(5,5,add));
+
+//function callbacks - 3
