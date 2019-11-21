1ec6fde33d11434b1968dc545d4da47bc08e9500
unknown <rajmohan2695@gmail.com>
2019-11-21 06:59:40 +0530

    Math.random and Math.floor

---
+++ Code_Workouts/JavascriptWorkouts/InBuiltFunctions.js
@@ -0,0 +1,8 @@
+//inBuiltFunctions
+//Math
+var a = Math.random();		// stores a random number between 0 and 1
+a = a*6;					// To store a random number between 0 and 6
+a = Math.floor(a);			// To make the random decimal number into a whole number
+a = a+1;					// To get a number from 1 to 6 (can be used to get a random value for dice game)
+console.log(a);
+console.log(Math.floor(Math.random()*6)+1);	//The whole function can be rewritten like this
