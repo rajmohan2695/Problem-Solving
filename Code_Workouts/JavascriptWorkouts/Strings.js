0b06e5db2b1b1a9e1abd28c3075fb3497cd88b78
unknown <rajmohan2695@gmail.com>
2019-11-27 07:51:20 +0530

    Different behaviour of console log while handling Strings and numbers

---
+++ Code_Workouts/JavascriptWorkouts/Strings.js
@@ -0,0 +1,8 @@
+// Javascript Strings
+
+//Printing strings in console
+console.log(15+6+"Hello");			// 21Hello
+
+console.log("Hello"+15+6);			// Hello156  (After a string the numbers are treated as strings)
+
+console.log(15+6+"Hello"+15+6)		// 21Hello156
