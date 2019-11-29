3b3052f07b82fd3efd7191651a2e92a007bf5c33
unknown <rajmohan2695@gmail.com>
2019-11-29 14:32:08 +0530

    Random push

--- Code_Workouts/JavascriptWorkouts/Looping.js
+++ Code_Workouts/JavascriptWorkouts/Looping.js
@@ -24,6 +24,43 @@ l
 d
 */
 
+//forEach loop
+function add(args){
+j=0;    
+args.forEach((i,k,l,m)=>{
+    j+=i;    
+	console.log(m);
+})
+console.log(j);
+}
+add(1,2,3,4,5);
+
+//forin
+function add(...args){
+for(let i in args){
+    console.log(i);    
+}
+}
+add(1,2,3,4,5);
+
+/* Output
+	0 
+	1 
+	2 
+	3 
+	4
+*/
+
+//forof							// It is used to iterate all the elements present inside the function
+function add(args){
+j=0;    
+for(let i of args){
+    j+=i;    
+}
+console.log(j);					// 15
+}
+add([1,2,3,4,5]);
+
 /*---------------------------------------------------------------*/
 
 //while loop
