342e7e46d32496e386df528982ba17bae5b48dcd
unknown <rajmohan2695@gmail.com>
2019-12-02 13:53:19 +0530

    Numeric type conversions

--- Code_Workouts/JavascriptWorkouts/TypeConversions.js
+++ Code_Workouts/JavascriptWorkouts/TypeConversions.js
@@ -1,12 +1,41 @@
 //Type Conversions
 
 //String Conversions 
+let a = true;
+console.log(typeof a);			// boolean
 
-let value = true;
-console.log(typeof value);			// boolean
-
-value = String(value);				// Now the value is converted to a string "true"
-console.log(typeof value);			// String
+a = String(a);					// Now the value of 'a' is converted to a string "true"
+console.log(typeof a);			// string
 
 /*---------------------------------------------------------------*/
 
+//Numeric Conversions
+let b = "11";
+console.log(typeof b);			// string
+b = Number(b);
+console.log(typeof b);			// number
+
+let c = "Hello";
+console.log(typeof c);			// string
+c = Number(c);
+console.log(c);					// NaN
+
+//Numeric Conversions - 2
+console.log("11"-2);			// 9
+console.log(typeof (12 -"11"));	// number
+console.log(typeof "11"-2);		// NaN
+
+//Numeric Conversions - 3
+console.log("10"/2);			// 5
+console.log(10/"2");			// 5
+console.log("10"/"2");			// 5
+
+//Numeric Conversions - 4
+console.log(Number(""));			// 0
+console.log(Number(" "));			// 0
+console.log(Number(true));			// 1
+console.log(Number(false));			// 0
+console.log(Number(null));			// 0
+console.log(Number(undefined));		// NaN
+
+/*---------------------------------------------------------------*/
