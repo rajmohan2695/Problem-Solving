03f5d2d3181201ad45e45ef3880628b66cc14e53
unknown <rajmohan2695@gmail.com>
2019-12-02 12:45:18 +0530

    Type conversions

---
+++ Code_Workouts/JavascriptWorkouts/TypeConversions.js
@@ -0,0 +1,12 @@
+//Type Conversions
+
+//String Conversions 
+
+let value = true;
+console.log(typeof value);			// boolean
+
+value = String(value);				// Now the value is converted to a string "true"
+console.log(typeof value);			// String
+
+/*---------------------------------------------------------------*/
+
