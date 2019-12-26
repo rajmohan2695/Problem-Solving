7deb2e00d857ec2c721550e4e0720a22e892e1b9
unknown <rajmohan2695@gmail.com>
2019-12-26 09:34:14 +0530

    Worked on call apply and bind

--- Code_Workouts/JavascriptWorkouts/call-apply-bind.js
+++ Code_Workouts/JavascriptWorkouts/call-apply-bind.js
@@ -12,6 +12,25 @@ var a = addFunction.call(obj,1,2);
 
 console.log(a); // 5
 
+    //      ----x----
+
+    let mammal = function(legs){
+        this.legs = legs;
+    }
+
+    let cat = function(legs,isDomesticated){
+        mammal.call(this,legs);
+        this.domesticated = isDomesticated;
+    }
+
+    let lion = new cat(4,false);    
+
+    console.log(lion);                  // cat {legs: 4, domesticated: false}
+
+    let tiger = new cat(5,true);
+
+    console.log(tiger);                 // cat {legs: 5, domesticated: true}
+
 /*---------------------------------------------------------------*/
 
 // using apply
