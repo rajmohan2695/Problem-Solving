7d46824417ac34465b552eb56e2915624c965c2c
Raj Mohan <rajmohan2695@gmail.com>
2020-01-29 20:20:52 +0530

    Decrypting the file and putting all the data into a properties object

--- Code_Workouts/JavaWorkouts/GoogleKMS/CommandLineDecryptionGoogleKMS.java
+++ Code_Workouts/JavaWorkouts/GoogleKMS/CommandLineDecryptionGoogleKMS.java
@@ -1,8 +1,10 @@
 package Code_Workouts.JavaWorkouts.GoogleKMS;
+import java.io.*;
+import java.util.Properties;
 import java.util.Scanner;
 
 //Created decryption process for GoogleKMS using commandLine through Java
-public class CommandLineEncryption {
+public class CommandLineDecryptionGoogleKMS {
 
         public static void main(String[] args) throws Exception {
             Scanner scan = new Scanner(System.in);
@@ -25,9 +27,32 @@ public class CommandLineEncryption {
 
             ProcessBuilder builder = new ProcessBuilder(
                     "cmd.exe", "/c", cmd);
-            builder.start();
+            Process process = builder.start();
+            process.waitFor();
             System.out.println("The file is decrypted and saved in desktop");
+
+            // ----------------------------
+
+            System.out.println("Here are the decrypted information from the file");
+            File file = new File("C:\\Users\\raj mohan\\Desktop\\"+decryptedFile+".txt");
+            getCredentials(file);
+
     }
+    public static void getCredentials(File file){
+        try (InputStream input = new FileInputStream(file)) {
+
+            Properties prop = new Properties();
+
+            prop.load(input);
+
+            prop.keySet().forEach(x -> System.out.println(x+" has "+prop.get(x)));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
 }
 
 // Output:
@@ -46,4 +71,11 @@ public class CommandLineEncryption {
         Suggest a name for the decrypted file
         DecryptFileOne
         The file is decrypted and saved in desktop
+        Here are the decrypted information from the file
+        FULLSTORAGE_SIGNKEY has RrFIMxbZxEzY0sq1Kz89QpBYTQllOg9J
+        FULLAUTH_DEVCONSOLE_APIKEY has wN4hMuO.NJocuLD6z0TgEEB7MHCn1Khua6eLkgkH
+        FULLSTORAGE_API_KEY has ZoSw20N.IpnO1HazffEATjeGd2Cd4lXj9j799kh1
+        FULLAUTH_APP_ID has f185790b1f7dedb532d1770de0e170afb274f54f
+        FULLAUTH_CLIENT_ID has bf60d-939a41b5a49c306e70ac
+        FULLAUTH_CLIENT_SECRET has xFB5ujcx7tExkjmj3-VQicLe8uIDnFLyZfpkH07x
 */
