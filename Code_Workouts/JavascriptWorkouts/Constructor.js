3eea48d0d32b78e6b9f6e941d6453ed4d9ed4125
Raj Mohan <rajmohan2695@gmail.com>
2020-05-22 14:23:56 +0530

    Used instanceof in javascript

--- Code_Workouts/JavascriptWorkouts/Constructor.js
+++ Code_Workouts/JavascriptWorkouts/Constructor.js
@@ -17,3 +17,43 @@ let two = new SmallUser;   // Both new SmallUser() and new SmallUser are same if
 console.log( two.name );         // John
 
 /*---------------------------------------------------------------*/
+
+// Finding the instance of a contructor
+// Using instanceof
+// Dog Catcher function
+
+function dogCatcher(){
+
+}
+
+function Cat(name, color, weight){
+    this.name = name;
+    this.color = color;
+    this.weight = weight;
+}
+
+function Dog(name, color, weight){
+    this.name = name;
+    this.color = color;
+    this.weight = weight;
+}
+
+let mahe = new Cat('mahe','white',2);
+let lehe = new Dog('lehe','Black',3);
+let hehe = new Dog('hehe','Brown',3);
+let pehe = new Cat('pehe','Wheat',1.4);
+
+let dogs = [ mahe, lehe, hehe, pehe ];
+
+const name = obj => Object.keys(dogs)[0];
+
+for(let dog of dogs){
+    if(dog instanceof Dog){
+        console.log(`${dog.name} is a ${dog.color} coloured dog`)
+    }
+    else{
+        console.log(`${dog.name} is not a dog`)
+    }
+}
+
+/*---------------------------------------------------------------*/
