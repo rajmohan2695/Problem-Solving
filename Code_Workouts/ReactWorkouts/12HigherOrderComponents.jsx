9be5e20bcd85971c6d6bb77ac6d457d21109d73e
Raj Mohan <rajmohan2695@gmail.com>
2020-06-16 11:39:27 +0530

    Added some important things that should be taken care of while implementing HOC in react

--- Code_Workouts/ReactWorkouts/12HigherOrderComponents.jsx
+++ Code_Workouts/ReactWorkouts/12HigherOrderComponents.jsx
@@ -19,7 +19,10 @@ const withCounter = WrappedComponent => {
 
         render(){
             return(
-                <WrappedComponent count={this.state.count} incrementCounter={this.incrementCounter} />
+                <WrappedComponent count={this.state.count} 
+                incrementCounter={this.incrementCounter} 
+                {...this.props}       // This is very much important that the props from the Wrapped component will the passed to the updated component(withCounter component)
+                />
             )
         }
     }
@@ -40,7 +43,9 @@ class ClickCounter extends Component{
         )
     }
 }
+export default withCounter(ClickCounter)
 
+// Reusing the counter functionality with HigherOrderComponent
 class HoverCounter extends Component{
     render(){
         const { count, incrementCounter } = this. props;
@@ -49,3 +54,58 @@ class HoverCounter extends Component{
         )
     }
 }
+export default withCounter(HoverCounter)
+
+/*---------------------------------------------------------------*/
+
+// Passing arguments with components with Higher order component
+
+const withCounter = ( WrappedComponent, incrementValue ) => {
+    class withCounter extends Component{
+        constructor(props){
+            super(props)
+            this.state = {
+                count:0
+            }
+        }
+
+        incrementCounter(){
+            this.setState((prevState)=>{
+                count:prevState+incrementValue
+            })
+        }
+
+        render(){
+            return(
+                <WrappedComponent count={this.state.count} incrementCounter={this.incrementCounter} {...this.props} />
+            )
+        }
+    }
+
+    return withCounter;
+}
+
+export default withCounter;
+
+class ClickCounter extends Component{
+    render(){
+        const { count, incrementCounter } = this.props;
+        return(
+            <div>
+                <button onClick={incrementCounter}>Increment Counter : {count}</button>
+            </div>
+        )
+    }
+}
+export default withCounter(ClickCounter, 5)     // The counter increments by 5 everytime
+
+// Reusing the counter functionality with HigherOrderComponent
+class HoverCounter extends Component{
+    render(){
+        const { count, incrementCounter } = this. props;
+        return(
+            <h2 onMouseOver={incrementCounter}>Hover here to increment the counter : {count}</h2>
+        )
+    }
+}
+export default withCounter(HoverCounter, 10)    // The counter increments by 10 everytime
