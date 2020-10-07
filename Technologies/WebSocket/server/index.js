12a13a6a14b6b8a679d72231ff1425def96d7be4
Raj Mohan <rajmohan2695@gmail.com>
2020-10-07 07:56:42 +0530

    Checking websocket connection in client side

--- Technologies/WebSocket/server/index.js
+++ Technologies/WebSocket/server/index.js
@@ -2,6 +2,13 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket.Server({
     port: 4100
+}, () => {
+    console.log('Web socket initialized');
 });
 
-console.log('Websocket initialized at 4100');
+/**
+ *  Try this in CLIENT side console once the connection is initialized
+ * const ws = new WebSocket('ws://localhost:4100');
+ * ws.send('Hello Websocket');
+ * ws.send(JSON.stringify({hi: 'Hello'}));
+ */
