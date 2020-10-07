59fd6b31c0f68d19c9eaf43852a6484118609dbf
Raj Mohan <rajmohan2695@gmail.com>
2020-10-07 08:30:38 +0530

    Sending and receiving of messages via socket

--- Technologies/WebSocket/server/index.js
+++ Technologies/WebSocket/server/index.js
@@ -1,14 +1,21 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket.Server({
+const webSocketServer = new WebSocket.Server({
     port: 4100
-}, () => {
-    console.log('Web socket initialized');
 });
 
+webSocketServer.on('connection', (webSocket) => {
+    console.log('Connect Established');
+    webSocket.send('Connection established');
+    webSocket.on('message', (data) => {
+        console.log(data);
+        webSocket.send(JSON.stringify({Hello: "Hi"}));
+    })
+})
+
+// Sending and receiving messages via socket in CLIENT side
 /**
- *  Try this in CLIENT side console once the connection is initialized
- * const ws = new WebSocket('ws://localhost:4100');
- * ws.send('Hello Websocket');
- * ws.send(JSON.stringify({hi: 'Hello'}));
+ * let wss4 = new WebSocket('ws://localhost:4100');
+ * wss7.onmessage = event => { console.log(event) }
+ * wss4.send(JSON.stringify({ Hi: "Hello" }));
  */
