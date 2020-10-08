3958bd95c1a07120671911d9c59776ee91f15ce3
Raj Mohan <rajmohan2695@gmail.com>
2020-10-08 06:41:16 +0530

    Used clients for broadcast

--- Technologies/WebSocket/server/index.js
+++ Technologies/WebSocket/server/index.js
@@ -4,10 +4,8 @@ const webSocketServer = new WebSocket.Server({
     port: 4100
 });
 
-const ports = [];
 webSocketServer.on('connection', (webSocket) => {
-    ports.push(webSocket);
     webSocket.on('message', (data) => {
-        ports.forEach(socketCon => socketCon.send(data))
+        webSocketServer.clients.forEach(socketCon => socketCon.send(data))
     })
 })
