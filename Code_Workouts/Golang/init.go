d600b2a8be88cbf942d1cc44de0183df6f5fbedc
RajMohan <rajmohan2695@gmail.com>
2023-08-21 13:31:11 +0530

    Worked on go basics

--- Code_Workouts/Golang/init.go
+++ Code_Workouts/Golang/init.go
@@ -3,5 +3,62 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// print sample
+	fmt.Print("Hello, World!")
+
+	print("\n")
+
+	// if else
+	a := "10"
+	if a == "10" {
+		fmt.Printf("On Control")
+	} else {
+		fmt.Printf("Off Control")
+	}
+
+	print("\n")
+
+	// if else - Initialize variable inside if
+	if a := "10"; a == "10" {
+		fmt.Printf("On Control")
+	} else {
+		fmt.Printf("Off Control")
+	}
+
+	print("\n")
+
+	// if else - multiple conditions
+	number := 10
+	if number > 9 {
+		print("Hello")
+	} else if 0 < number && number < 9 {
+		print("Between 0 and 9")
+	} else {
+		print("Bye")
+	}
+
+	print("\n")
+
+	// for - traditional way
+	for a := 1; a < 10; a++ {
+		fmt.Printf("%d", a)
+	}
+
+	print("\n")
+
+	// for - with count
+	var b = 1
+	for b < 10 {
+		fmt.Printf("%d", b)
+		b++
+	}
+
+	print("\n")
+
+	// for - infite loop
+	for {
+		print("Infinite loop")
+		break
+	}
+
 }
