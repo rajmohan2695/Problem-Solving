53e17ef0e552ed5109d70e8aab65c6bc07e50eb8
RajMohan <rajmohan2695@gmail.com>
2024-04-12 11:09:26 +0530

    Added receiver function

--- Code_Workouts/Golang/topics/Struct.go
+++ Code_Workouts/Golang/topics/Struct.go
@@ -44,20 +44,47 @@ func printEmployeeDetail() {
 	fmt.Printf("The Employee %s has %d years of experience. Currently he is working as %s\n", employee.User.FirstName, employee.ExperienceInYears, employee.Role)
 }
 
+// Receiver funtion
+
+func (emp *Employee) getEmployeeRoleAndExperienceAsString() string {
+	return fmt.Sprintf("The Employee %s has %d years of experience. Currently he is working as %s\n", emp.User.FirstName, emp.ExperienceInYears, emp.Role)
+}
+
+func printEmployeeDetailUsingReceiverFuntion() {
+	fmt.Println("\n - Receiver function -")
+	employee := Employee{
+		Role:              "Staff Software Engineer",
+		ExperienceInYears: 8,
+		User: User{
+			FirstName: "Raj Mohan",
+			LastName:  "Jayakumar",
+			Age:       28,
+		},
+	}
+	fmt.Println(employee)
+	fmt.Println(employee.getEmployeeRoleAndExperienceAsString())
+}
+
 func Struct() {
 	fmt.Println("\nStruct:")
 	printUserDetail()
 	printEmployeeDetail()
+	printEmployeeDetailUsingReceiverFuntion()
 }
 
 /*
 
 Output:
 
+Struct:
 {Raj Mohan Jayakumar 28}
 My User is Raj Mohan Jayakumar, And he/she is 28 years old
 
  - Nested Structs -
+{Staff Software Engineer 8 {Raj Mohan Jayakumar 28}}
+The Employee Raj Mohan has 8 years of experience. Currently he is working as Staff Software Engineer
+
+ - Receiver function -
 {Staff Software Engineer 8 {Raj Mohan Jayakumar 28}}
 The Employee Raj Mohan has 8 years of experience. Currently he is working as Staff Software Engineer
 
