fd3b09a1a8d149a9f5e92f425d6f7d531ea49c96
RajMohan <rajmohan2695@gmail.com>
2024-04-12 10:06:18 +0530

    Added output for pointers

--- Code_Workouts/Golang/topics/Pointers.go
+++ Code_Workouts/Golang/topics/Pointers.go
@@ -15,9 +15,24 @@ func withPointers(argument *string) {
 }
 
 func Pointers() {
+	fmt.Println("\nPointers:")
 	var argument string = "Original Value"
 	withoutPointers(argument)
-	fmt.Println("Printing from main function after withoutPointers function call: " + argument)
+	fmt.Println("Printing from main function after 'withoutPointers' function call: " + argument)
+	fmt.Println()
 	withPointers(&argument)
-	fmt.Println("Printing from main function after withPointers function call: " + argument)
+	fmt.Println("Printing from main function after 'withPointers' function call: " + argument)
 }
+
+/*
+Output:
+
+Pointers:
+Function without pointer, Value = Original Value
+Printing inside called function after change of value: Different Value
+Printing from main function after 'withoutPointers' function call: Original Value
+
+Function with pointer, Value = Original Value
+Printing inside called function after change of value: Different Value
+Printing from main function after 'withPointers' function call: Different Value
+*/
