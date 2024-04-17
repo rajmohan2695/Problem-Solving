8dabf0a6f5007e7f5c1a60fdfc9bed16cea8f5b8
RajMohan <rajmohan2695@gmail.com>
2024-04-17 16:20:52 +0530

    Added update and add new todo

--- Code_Workouts/Golang/topics/TodoApi.go
+++ Code_Workouts/Golang/topics/TodoApi.go
@@ -2,12 +2,13 @@ package topics
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type todo struct {
-	Id          string `json:"id"`
+	Id          int    `json:"id"`
 	Title       string `json:"title"`
 	IsCompleted bool   `json:"is_completed"`
 }
@@ -18,9 +19,9 @@ type error struct {
 }
 
 var todoList = []todo{
-	{Id: "1", Title: "Clean Room", IsCompleted: false},
-	{Id: "2", Title: "Dry Clothes", IsCompleted: false},
-	{Id: "3", Title: "Learn Go", IsCompleted: false},
+	{Id: 1, Title: "Clean Room", IsCompleted: false},
+	{Id: 2, Title: "Dry Clothes", IsCompleted: false},
+	{Id: 3, Title: "Learn Go", IsCompleted: false},
 }
 
 func getAllTodos(context *gin.Context) {
@@ -28,7 +29,12 @@ func getAllTodos(context *gin.Context) {
 }
 
 func updateTodo(context *gin.Context) {
-	todoId := context.Param("id")
+	todoIdStr := context.Param("id")
+	todoId, err := strconv.Atoi(todoIdStr)
+	if err != nil {
+		context.IndentedJSON(http.StatusBadRequest, "Invalid ID")
+		return
+	}
 	var updateTodoValue todo
 	if err := context.BindJSON(&updateTodoValue); err != nil {
 		context.IndentedJSON(http.StatusBadRequest, error{StatusCode: http.StatusBadRequest, Message: "Bad Request"})
@@ -41,13 +47,24 @@ func updateTodo(context *gin.Context) {
 			break
 		}
 	}
-	context.IndentedJSON(http.StatusAccepted, "Todo Updated Successfully")
+	context.IndentedJSON(http.StatusOK, "Todo Updated Successfully")
+}
 
+func addTodo(context *gin.Context) {
+	var newTodo todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		context.IndentedJSON(http.StatusBadRequest, error{StatusCode: http.StatusBadRequest, Message: "Bad Request"})
+		return
+	}
+	newTodo.Id = len(todoList) + 1
+	todoList = append(todoList, newTodo)
+	context.IndentedJSON(http.StatusOK, todoList)
 }
 
 func TodoApiInit() {
 	router := gin.Default()
 	router.GET("/todos", getAllTodos)
+	router.POST("/todo", addTodo)
 	router.PATCH("/todo/:id", updateTodo)
 	router.Run("localhost:8181")
 }
